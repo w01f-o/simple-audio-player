24d1345f50087b4c004e001b8a00ac2e43fb35d1
w01f <arsenimarchenko@bk.ru>
2024-05-16 16:45:00 +0300

    ready

---
+++ src/assets/img/covers/covers.ts
@@ -0,0 +1,32 @@
+import trackCoverElizar1 from "./1elizar.png";
+import trackCoverElizar2 from "./2elizar.png";
+import trackCoverElizar3 from "./3elizar.png";
+import trackCoverElizar4 from "./4elizar.png";
+import trackCoverElizar5 from "./5elizar.png";
+import trackCoverElizar6 from "./6elizar.png";
+import trackCoverElizar7 from "./7elizar.png";
+import trackCoverElizar8 from "./8elizar.png";
+import trackCoverElizar9 from "./9elizar.png";
+import trackCoverVadim1 from "./1vadim.png";
+import trackCoverVadim2 from "./2vadim.png";
+import trackCoverVadim3 from "./3vadim.png";
+import trackCoverVadim4 from "./4vadim.png";
+
+export const elizarTrackCoversArray: string[] = [
+  trackCoverElizar1,
+  trackCoverElizar2,
+  trackCoverElizar3,
+  trackCoverElizar4,
+  trackCoverElizar5,
+  trackCoverElizar6,
+  trackCoverElizar7,
+  trackCoverElizar8,
+  trackCoverElizar9,
+];
+
+export const vadimTrackCoversArray: string[] = [
+  trackCoverVadim1,
+  trackCoverVadim2,
+  trackCoverVadim3,
+  trackCoverVadim4,
+];
