9b21cd93b311615cbde99eac991186db77518e39
w01f <arsenimarchenko@bk.ru>
2024-05-16 17:23:43 +0300

    ready

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import autoprefixer from "autoprefixer";
+import pxtorem from "postcss-pxtorem";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -19,7 +20,7 @@ export default defineConfig({
   },
   css: {
     postcss: {
-      plugins: [autoprefixer({})],
+      plugins: [autoprefixer({}), pxtorem({ replace: true, propList: ["*"] })],
     },
   },
 });
